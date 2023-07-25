c44f4f58796c3de7e9f438f29b52e37f3c470138
kblazheev <blazheev.k@gmail.com>
2023-07-25 14:17:51 +0300

    Changes to be committed: 	modified:   constants.py 	modified:   hw3.py 	modified:   logger.conf

--- constants.py
+++ constants.py
@@ -1,19 +1,6 @@
-okved_web = 'https://ofdata.ru/open-data/download/okved_2.json.zip'
-okved_local = '/home/kirill/okved_2.json.zip'
-okved_file = 'okved_2.json'
 egrul_web = 'https://ofdata.ru/open-data/download/egrul.json.zip'
 egrul_local = '/home/kirill/egrul.json.zip'
 db = 'hw3.db'
-tab_okved = '''
-    CREATE TABLE IF NOT EXISTS okved(
-        id integer primary key,
-        code text,
-        parent_code text,
-        section text,
-        name text,
-        comment text
-    );'''
-insert_okved = "INSERT INTO okved(code, parent_code, section, name, comment) VALUES(?, ?, ?, ?, ?);"
 tab_telecom = '''
     CREATE TABLE IF NOT EXISTS telecom_companies(
         id integer primary key,
@@ -43,7 +30,6 @@ tab_skills_top = '''
             );'''
 insert_skill = "INSERT INTO skills_top(count, name) VALUES(?, ?);"
 url = "https://api.hh.ru/vacancies"
-url2 = "https://hh.ru/search/vacancy"
 api_url = "https://api.hh.ru/"
 user_agent = {'User-agent': 'Mozilla/5.0'}
 url_params = {
--- hw3.py
+++ hw3.py
@@ -4,10 +4,10 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
-from airflow.providers.sqlite.operators.sqlite import SqliteOperator
 from airflow.providers.sqlite.hooks.sqlite import SqliteHook
 import requests
 import asyncio
+import sqlite3 as sql
 import time
 from aiohttp import ClientSession as cs
 from aiohttp import TCPConnector as tcpcn
@@ -18,6 +18,7 @@ from pathlib import Path
 import zipfile
 import pandas as pd
 from normalizer import normal
+from collections import Counter
 
 config = Path('/home/kirill/airflow/dags/logger.conf')
 logging.config.fileConfig(fname=config, disable_existing_loggers=False)
@@ -31,25 +32,45 @@ default_args = {
 
 def telecoms(**kwargs):
     try:
-        with zipfile.ZipFile(okved_local, 'r') as zip:
-            with zip.open(okved_file) as file:
-                okved_df = pd.read_json(file)
-        logger.info(f"Данные ОКВЭД успешно загружены из архива {okved_local}")
-        sh = SqliteHook(sqlite_conn_id='hw3')  
-        sh.run(tab_okved)
-        logger.info("Таблица okved успешно добавлена")
-        data = []
-        for index, row in okved_df.iterrows():
-            data.append(row)
-        fields = ['code', 'parent_code', 'section', 'name', 'comment']
-        sh.insert_rows(
-            table='okved',
-            rows=data,
-            target_fields=fields,
-        )
-        logger.info("Данные успешно добавлены в таблицу okved")
+        sh = SqliteHook(sqlite_conn_id='hw3')
+        connection = sh.get_conn()
+        cursor = connection.cursor()
+        logger.info(f"База данных {db} подключена")
+        cursor.execute(tab_telecom)
+        connection.commit()
+        logger.info("Таблица telecom_companies успешно добавлена в БД")
+        with zipfile.ZipFile(egrul_local, 'r') as zip:
+            names = zip.namelist()
+            for name in names:
+                data = []
+                with zip.open(name) as file:
+                    egrul = pd.read_json(file)
+                    for index, row in egrul.iterrows():
+                        ogrn_date = ''
+                        if 'ДатаОГРН' in row['data'].keys():
+                            ogrn_date = row['data']['ДатаОГРН']
+                        if 'СвОКВЭД' in row['data'].keys():
+                            if 'СвОКВЭДОсн' in row['data']['СвОКВЭД'].keys():
+                                okved = row['data']['СвОКВЭД']['СвОКВЭДОсн']['КодОКВЭД']
+                                if okved[:2] == '61':
+                                    data.append([row['name'], normal(row['name']), row['inn'], row['ogrn'], ogrn_date, okved])
+                    try:
+                        cursor.executemany(insert_telecom, data)
+                        connection.commit()
+                        logger.info(f"Данные файла {name} успешно добавлены в таблицу telecom_companies")
+                    except sql.Error as error:
+                        logger.error(f"Не удалось добавить данные файла {name} в таблицу telecom_companies")
+                        logger.error(f"Исключение: {error.__class__}, {error.args}")
+        cursor.close()
+    except sql.Error as error:
+        logger.critical("Не удалось добавить таблицу telecom_companies")
+        logger.critical(f"Исключение: {error.__class__}, {error.args}")
     except Exception:
-        logger.critical(f"Ошибка чтения архива {okved_local}")
+        logger.critical(f"Ошибка чтения архива {egrul_local}")
+    finally:                    
+        if (connection):
+            connection.close()
+            logger.info("Соединение закрыто")
 
 async def get_vacancy(url, session):
     async with session.get(url=url) as vacancy_page:
@@ -85,19 +106,63 @@ def vacancies(**kwargs):
             asyncio.run(main(vacancy_list, data))
             logger.info(f"Время выполнения, с: {time.time() - start}")
             sh = SqliteHook(sqlite_conn_id='hw3')
-            sh.run(tab_vacancies)
-            logger.info('Таблица vacancies успешно добавлена в БД')
-            fields = ['employer_name', 'normal_name', 'name', 'description', 'key_skills']
-            sh.insert_rows(
-                table='vacancies',
-                rows=data,
-                target_fields=fields,
-            )
-            logger.info('Данные успешно добавлены в таблицу vacancies')
+            try:
+                connection = sh.get_conn()
+                cursor = connection.cursor()
+                logger.info(f"База данных {db} подключена")
+                cursor.execute(tab_vacancies)
+                connection.commit()
+                logger.info('Таблица vacancies успешно добавлена в БД')
+                cursor.executemany(insert_vacancy, data)
+                connection.commit()
+                logger.info('Данные успешно добавлены в таблицу vacancies')
+                cursor.close()
+            except sql.Error as error:
+                logger.error('Не удалось вставить данные в таблицу vacancies')
+                logger.error("Исключение: ", error.__class__, error.args)
+            finally:
+                if (connection):
+                    connection.close()
+                    logger.info("Соединение с закрыто")
+        else:
+            logger.critical(f"Не удалось загрузить данные поиска: {search_result.status_code}")
     except Exception:
-        logger.critical(f"Не удалось загрузить данные поиска: {search_result.status_code}")
         logger.critical(f"Исключение: {Exception.__class__}, {Exception.args}")
-        
+
+def skills(**kwargs):
+    try:
+        sh = SqliteHook(sqlite_conn_id='hw3')
+        connection = sh.get_conn()
+        cursor = connection.cursor()
+        logger.info(f"База данных {db} подключена")
+        joiner = """
+            SELECT vacancies.employer_name, vacancies.key_skills, telecom_companies.name, max(telecom_companies.okved)
+            FROM vacancies INNER JOIN telecom_companies
+            ON telecom_companies.normal_name LIKE vacancies.normal_name
+            GROUP BY vacancies.employer_name;"""
+        cursor.execute(joiner)
+        skills = []
+        for item in cursor.fetchall():
+            skills.extend(item[1].replace(' ', '').split(','))
+            logger.info(item)
+        c = Counter(skills)
+        cursor.execute(tab_skills_top)
+        connection.commit()
+        logger.info('Таблица skills_top успешно добавлена в БД')
+        for skill in c.most_common(10):
+            cursor.execute(insert_skill, [skill[1], skill[0]])
+            logger.info(f"{str(skill[1]).rjust(2)} - {skill[0]}")
+        connection.commit()
+        logger.info('Данные успешно добавлены в таблицу skills_top')
+        cursor.close()
+    except sql.Error as error:
+        logger.critical("Не удалось выполнить запрос")
+        logger.critical(f"Исключение: {error.__class__}, {error.args}")
+    finally:                    
+        if (connection):
+            connection.close()
+            logger.info("Соединение закрыто")
+
 with DAG(
     dag_id='hw3',
     default_args=default_args,
@@ -105,16 +170,20 @@ with DAG(
     start_date=datetime(2023, 7, 22),
     schedule='@daily'
 ) as dag:
-    download_egrul = BashOperator(
-        task_id='download_egrul',
-        bash_command=f"wget {okved_web} -O {okved_local}"
-    )
+    # download_egrul = BashOperator(
+    #     task_id='download_egrul',
+    #     bash_command=f"wget {egrul_web} -O {egrul_local}"
+    # )
     load_telecoms = PythonOperator(
         task_id='load_telecoms',
         python_callable=telecoms,
     )
-    get_vacancies = PythonOperator(
-        task_id='get_vacancies',
-        python_callable=vacancies,
-    )
-    download_egrul >> load_telecoms >> get_vacancies
+    # get_vacancies = PythonOperator(
+    #     task_id='get_vacancies',
+    #     python_callable=vacancies,
+    # )
+    # top_skills = PythonOperator(
+    #     task_id='top_skills',
+    #     python_callable=skills,
+    # )
+    # download_egrul >> load_telecoms >> get_vacancies >> top_skills
